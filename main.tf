574dab9ba381e0e712c854ac102cd31347c558ce
Josh Campbell <josh@userdel.com>
2016-01-25 12:46:26 -0600

    removes keys

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
   region = "${var.region}"
 }
-
 module "site" {
   source = "./site"
   key_name = "${var.key_name}"
   ip_range = "${var.ip_range}"
 }
-
 module "launch_configurations" {
   source = "./launch_configurations"
   webapp_http_inbound_sg_id = "${module.site.webapp_http_inbound_sg_id}"

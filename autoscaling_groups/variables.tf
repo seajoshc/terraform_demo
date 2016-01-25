0236ade78972fe8080d88127b08ddc6960ffa3e9
Josh Campbell <josh@userdel.com>
2016-01-25 13:00:56 -0600

    removes whitespace

--- autoscaling_groups/variables.tf
+++ autoscaling_groups/variables.tf
@@ -2,15 +2,12 @@ variable "availability_zones" {
   # No spaces allowed between az names!
   default = "us-west-2a,us-west-2b,us-west-2c"
 }
-
 variable "asg_min" {
   default = "2"
 }
-
 variable "asg_max" {
   default = "10"
 }
-
 #
 # From other modules
 #
